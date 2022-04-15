55216bd902503e35824c421fbf0e65a4967730a0
adeveloper-wq <dajesch@gmail.com>
2022-04-15 02:36:47 +0200

    Test Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,11 @@
 import Layout from '../components/Layout'
-/* import MapComponent from '../components/HeatMap'; */
+import MapComponent from '../components/HeatMap';
 import axios from 'axios'
 import { Date } from '../interfaces';
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
-import dynamic from 'next/dynamic';
 
 const IndexPage = () => {
-  const MapComponent = dynamic(() => import("../components/HeatMap"), {
-    loading: () => <p>Loading...</p>,
-    ssr: false
-  });
-
   const [dates, setDates] = useState<Array<Date>>(undefined);
 
   let mapboxKey = process.env.NEXT_PUBLIC_REACT_APP_MAPBOX_ACCESS_TOKEN;
